37145457d062937c39e101944bf18d045b7a18e9
mamungithube <orroshidmdmamun50@gmail.com>
2025-05-02 08:55:01 +0600

    update

--- chat_system/settings.py
+++ chat_system/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://chat-app-9tq4.onrender.com','https://*.127.0.0.1']
-
+CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
